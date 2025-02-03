9e179eb5fb7a3f3345cc83f0ae4fc37901b25a1a
Subrina Lisa <subrinalisa5@gmail.com>
2025-02-03 18:04:42 +0600

    all

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,44 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        devoptions: {
+          enabled: true
+        },
+
+        // includeAssets: ['favicon.ico', 'robots.txt'],
+        manifest: {
+          name: 'PBI Dashboard',
+          short_name: 'PBI',
+          description: 'A description of PBI',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'icons/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'icons/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+        workbox: {
+          maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // Increase the limit to 10 MiB
+        },
+      }
+    ),
+
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
