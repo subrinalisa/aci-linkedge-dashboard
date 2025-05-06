f2350b1b757d32548445e6e71a85b8c8fa82c4c6
Subrina Lisa <subrinalisa5@gmail.com>
2025-05-06 12:28:24 +0600

    base url update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import Cookies from "js-cookie";
 // export const apiBase = "https://linkage-backend.mis.digital/report-management-backend/api";
-export const apiBase = "https://webapps.acibd.com/report-management-backend/api";
+// export const apiBase = "https://webapps.acibd.com/report-management-backend/api";
+export const apiBase = "https://wa.acibd.com/report-management-backend/api";
 
 export const config = {
     headers: {
